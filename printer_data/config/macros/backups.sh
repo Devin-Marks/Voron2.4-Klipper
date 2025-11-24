e2ba231ff0abdc2cf20208d878696333af5d3e48
voron <voron@voron-4b17078>
2025-11-24 11:22:04 -0500

    Cron backup - 24/11/25 - 11:22:01

--- printer_data/config/macros/backups.sh
+++ printer_data/config/macros/backups.sh
@@ -1,9 +1,9 @@
-cp /home/voron/klipper-backup/.env /home/voron/printer_data/config/.env.bak
-cp /home/voron/fluidd-config/fluidd.cfg /home/voron/printer_data/config/fluidd.cfg.bak
-cp /var/spool/cron/crontabs/voron /home/voron/printer_data/config/crontab.bak
+cp /home/voron/klipper-backup/.env /home/voron/printer_data/config/backups/.env.bak
+cp /home/voron/fluidd-config/fluidd.cfg /home/voron/printer_data/config/backups/fluidd.cfg.bak
+cp /var/spool/cron/crontabs/voron /home/voron/printer_data/config/backups/crontab.bak
 
-chown voron:voron /home/voron/printer_data/config/.env.bak
-chown voron:voron /home/voron/printer_data/config/fluidd.cfg.bak
-chown voron:voron /home/voron/printer_data/config/crontab.bak
+chown voron:voron /home/voron/printer_data/config/backups/.env.bak
+chown voron:voron /home/voron/printer_data/config/backups/fluidd.cfg.bak
+chown voron:voron /home/voron/printer_data/config/backups/crontab.bak
 
-sed -i '1d' /home/voron/printer_data/config/.env.bak
+sed -i '1d' /home/voron/printer_data/config/backups/.env.bak
